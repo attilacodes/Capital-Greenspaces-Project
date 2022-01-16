6f62b4c3bb994424bc1d43823c9902c0b55121bc
Attila Lukacs <81167965+attilacodes@users.noreply.github.com>
2022-01-16 22:04:29 +0000

    updated files

--- main.py
+++ main.py
@@ -5,7 +5,4 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    #run app on port 55402
-    app.run(port=55402, debug=True)
-    #accessing app via built-in developer server
-    #app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=55402, debug=True)
