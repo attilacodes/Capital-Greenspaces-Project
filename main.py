8fb5e86575befc6cb50e1b1fbeff28f4b16c1116
VictoriaS99 <65685110+VictoriaS99@users.noreply.github.com>
2022-01-16 21:14:37 +0000

    Final edits

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #run app on port 55402
+    app.run(port=55402, debug=True)
+    #accessing app via built-in developer server
     #app.run(host='0.0.0.0', port=8000, debug=True)
